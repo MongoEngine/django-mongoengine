2e04899cf39c88622e6e5646c6ebc05a12956960
mvergerdelbove <mvergerdelbove@work4labs.com>
2012-08-23 19:30:28 +0200

    subdict support
    should work, 'id' machinery is working

--- django_mongoengine/admin/static/dict.js
+++ django_mongoengine/admin/static/dict.js
@@ -8,14 +8,13 @@ $(document).ready(function(){
 		var id = $(this).attr('id');
 		dictionaries[id] = $(this);
 		prefixes[id] = id.substring(0,id.length-1);
-		var next_id = get_next_id($(this).find('li:last-child input:first-child').attr('id'),prefixes[id]);
+		var next_id = get_next_id($(this).children('li:last-child').children('input:first-child').attr('id'),prefixes[id]);
 		next_els[id] = get_next_el(next_id,'pair');
 	});
-
+	console.log('global > ');
+	console.log(dictionary_ul);
 	function get_next_id(id, prefix){
-		/*
-		Support for default pair dictionaries
-		*/
+		console.log('get_next_id > Entering : '+ id + ', ' + prefix)
 		var u = id.substring(prefix.length);
 		/*console.log(id);
 		console.log(prefix);
@@ -23,19 +22,45 @@ $(document).ready(function(){
 		if ((i = u.search('pair')) > 0){
 			return prefix + (parseInt(u.substring(0,i))+1)+'_pair_';
 		}
-		console.log('Failed : other type');
+		else if ((i = u.search('subdict')) > 0){
+			return prefix + (parseInt(u.substring(0,i))+1)+'_pair_';
+		}
+		console.log('get_next_id > Passed trough : '+ id + ', ' + prefix)
+		//console.log(id);
+		//console.log(prefix);
 	}
 
 	function get_next_el(next_id, type){
+		console.log('get_next_el > ' + next_id);
 		if (type == 'pair'){
 			return '<li><input type="text" name="'+ next_id.substring(3) +'0" id="'+ next_id +'0"/> : <input type="text" name="'+ next_id.substring(3) +'1" id="'+ next_id +'1"/></li>';
 		}
 	}
 
+	/*
+	Add a field to a dictionary or subdictionary
+	*/
+
 	$('.add_dictionary').click(function(){
 		var id = $(this).attr('id').substring(4);
+		console.log('click_function > '+ id);
 		dictionaries[id].append(next_els[id]);
 		var next_id = get_next_id(dictionaries[id].find('li:last-child input:first-child').attr('id'),prefixes[id]);
-		next_els[id] = get_next_el(next_id, 'pair');
+		if ((next_el = get_next_el(next_id, 'pair')) == next_els[id]){
+			console.log('click_function > SAME ELS');
+			console.log('click_function > ' + dictionaries[id].children('li:last-child').children('input:first-child').attr('id'));
+		}
+		else {
+			next_els[id] = next_el;
+		}
+	});
+
+	/*
+	Add a subdictionary to a dictionary or subdictionary
+	*/
+
+	$('.add_sub_dictionary').click(function(){
+		var id = $(this).attr('id').substring(8);
+		
 	});
 });
