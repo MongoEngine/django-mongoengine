81e1e581959df2b85da31d30dbf365b8cbcb1d18
mvergerdelbove <mvergerdelbove@work4labs.com>
2012-09-03 12:40:55 +0200

    copy-paste and modif of the django.contrib.admin.helpers
    need to refactor code and look for possible inheritance

--- django_mongoengine/admin/helpers.py
+++ django_mongoengine/admin/helpers.py
@@ -1,6 +1,20 @@
+from django import forms
+from django.contrib.admin.util import lookup_field
 from django.contrib.admin.helpers import InlineAdminForm as DjangoInlineAdminForm
 from django.contrib.admin.helpers import InlineAdminFormSet as DjangoInlineAdminFormSet
-from django.contrib.admin.helpers import AdminForm
+from django.contrib.admin.helpers import normalize_fieldsets, AdminField
+from django.contrib.admin.templatetags.admin_static import static
+from django.core.exceptions import ObjectDoesNotExist
+#from django.db.models.fields.related import ManyToManyRel
+from django.forms.util import flatatt
+from django.template.defaultfilters import capfirst
+from django.utils.encoding import force_unicode, smart_unicode
+from django.utils.html import escape, conditional_escape
+from django.utils.safestring import mark_safe
+from django.conf import settings
+
+from django_mongoengine.admin.util import display_for_field, label_for_field, help_text_for_field
+
 
 class InlineAdminFormSet(DjangoInlineAdminFormSet):
     """
@@ -18,7 +32,6 @@ class InlineAdminFormSet(DjangoInlineAdminFormSet):
         yield InlineAdminForm(self.formset, self.formset.empty_form,
             self.fieldsets, self.opts.prepopulated_fields, None,
             self.readonly_fields, model_admin=self.opts)
-
 
 
 class InlineAdminForm(DjangoInlineAdminForm):
@@ -42,3 +55,153 @@ class InlineAdminForm(DjangoInlineAdminForm):
             return None
 
 
+class AdminForm(object):
+    def __init__(self, form, fieldsets, prepopulated_fields, readonly_fields=None, model_admin=None):
+        self.form, self.fieldsets = form, normalize_fieldsets(fieldsets)
+        self.prepopulated_fields = [{
+            'field': form[field_name],
+            'dependencies': [form[f] for f in dependencies]
+        } for field_name, dependencies in prepopulated_fields.items()]
+        self.model_admin = model_admin
+        if readonly_fields is None:
+            readonly_fields = ()
+        self.readonly_fields = readonly_fields
+
+    def __iter__(self):
+        for name, options in self.fieldsets:
+            yield Fieldset(self.form, name,
+                readonly_fields=self.readonly_fields,
+                model_admin=self.model_admin,
+                **options
+            )
+
+    def first_field(self):
+        try:
+            fieldset_name, fieldset_options = self.fieldsets[0]
+            field_name = fieldset_options['fields'][0]
+            if not isinstance(field_name, basestring):
+                field_name = field_name[0]
+            return self.form[field_name]
+        except (KeyError, IndexError):
+            pass
+        try:
+            return iter(self.form).next()
+        except StopIteration:
+            return None
+
+    def _media(self):
+        media = self.form.media
+        for fs in self:
+            media = media + fs.media
+        return media
+    media = property(_media)
+
+
+class Fieldset(object):
+    def __init__(self, form, name=None, readonly_fields=(), fields=(), classes=(),
+      description=None, model_admin=None):
+        self.form = form
+        self.name, self.fields = name, fields
+        self.classes = u' '.join(classes)
+        self.description = description
+        self.model_admin = model_admin
+        self.readonly_fields = readonly_fields
+
+    def _media(self):
+        if 'collapse' in self.classes:
+            extra = '' if settings.DEBUG else '.min'
+            js = ['jquery%s.js' % extra,
+                  'jquery.init.js',
+                  'collapse%s.js' % extra]
+            return forms.Media(js=[static('admin/js/%s' % url) for url in js])
+        return forms.Media()
+    media = property(_media)
+
+    def __iter__(self):
+        for field in self.fields:
+            yield Fieldline(self.form, field, self.readonly_fields, model_admin=self.model_admin)
+
+
+class Fieldline(object):
+    def __init__(self, form, field, readonly_fields=None, model_admin=None):
+        self.form = form  # A django.forms.Form instance
+        if not hasattr(field, "__iter__"):
+            self.fields = [field]
+        else:
+            self.fields = field
+        self.model_admin = model_admin
+        if readonly_fields is None:
+            readonly_fields = ()
+        self.readonly_fields = readonly_fields
+
+    def __iter__(self):
+        for i, field in enumerate(self.fields):
+            if field in self.readonly_fields:
+                yield AdminReadonlyField(self.form, field, is_first=(i == 0),
+                    model_admin=self.model_admin)
+            else:
+                yield AdminField(self.form, field, is_first=(i == 0))
+
+    def errors(self):
+        return mark_safe(u'\n'.join([self.form[f].errors.as_ul() for f in self.fields if f not in self.readonly_fields]).strip('\n'))
+
+
+class AdminReadonlyField(object):
+    def __init__(self, form, field, is_first, model_admin=None):
+        label = label_for_field(field, form._meta.model, model_admin)
+        # Make self.field look a little bit like a field. This means that
+        # {{ field.name }} must be a useful class name to identify the field.
+        # For convenience, store other field-related data here too.
+        if callable(field):
+            class_name = field.__name__ != '<lambda>' and field.__name__ or ''
+        else:
+            class_name = field
+        self.field = {
+            'name': class_name,
+            'label': label,
+            'field': field,
+            'help_text': help_text_for_field(class_name, form._meta.model)
+        }
+        self.form = form
+        self.model_admin = model_admin
+        self.is_first = is_first
+        self.is_checkbox = False
+        self.is_readonly = True
+
+    def label_tag(self):
+        attrs = {}
+        if not self.is_first:
+            attrs["class"] = "inline"
+        label = self.field['label']
+        contents = capfirst(force_unicode(escape(label))) + u":"
+        return mark_safe('<label%(attrs)s>%(contents)s</label>' % {
+            "attrs": flatatt(attrs),
+            "contents": contents,
+        })
+
+    def contents(self):
+        from django.contrib.admin.templatetags.admin_list import _boolean_icon
+        from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
+        field, obj, model_admin = self.field['field'], self.form.instance, self.model_admin
+        try:
+            f, attr, value = lookup_field(field, obj, model_admin)
+        except (AttributeError, ValueError, ObjectDoesNotExist):
+            result_repr = EMPTY_CHANGELIST_VALUE
+        else:
+            if f is None:
+                boolean = getattr(attr, "boolean", False)
+                if boolean:
+                    result_repr = _boolean_icon(value)
+                else:
+                    result_repr = smart_unicode(value)
+                    if getattr(attr, "allow_tags", False):
+                        result_repr = mark_safe(result_repr)
+            else:
+                if value is None:
+                    result_repr = EMPTY_CHANGELIST_VALUE
+                #HERE WE NEED TO CHANGE THIS TEST
+                # elif isinstance(f.rel, ManyToManyRel):
+                #     result_repr = ", ".join(map(unicode, value.all()))
+                else:
+                    result_repr = display_for_field(value, f)
+        return conditional_escape(result_repr)
