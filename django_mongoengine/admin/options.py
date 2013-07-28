f070962500df11023cb318792baab5d6387bd910
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-07-28 12:43:44 -0300

    Remove has_absolute_url, Reverse for 'view_on_site' with arguments requires contenttype (in Django 1.5)

--- django_mongoengine/admin/options.py
+++ django_mongoengine/admin/options.py
@@ -799,7 +799,8 @@ class DocumentAdmin(BaseDocumentAdmin):
         """
         return formset.save()
 
-    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+    def render_change_form(self, request, context, add=False, change=False,
+                           form_url='', obj=None):
         opts = self.model._admin_opts
         app_label = opts.app_label
         ordered_objects = opts.get_ordered_objects()
@@ -810,7 +811,7 @@ class DocumentAdmin(BaseDocumentAdmin):
             'has_change_permission': self.has_change_permission(request, obj),
             'has_delete_permission': self.has_delete_permission(request, obj),
             'has_file_field': True, # FIXME - this should check if form or formsets have a FileField,
-            'has_absolute_url': hasattr(self.model, 'get_absolute_url'),
+            #'has_absolute_url': hasattr(self.model, 'get_absolute_url'),
             'ordered_objects': ordered_objects,
             'form_url': mark_safe(form_url),
             'opts': opts,
@@ -819,10 +820,11 @@ class DocumentAdmin(BaseDocumentAdmin):
             'save_on_top': self.save_on_top,
             'root_path': self.admin_site.root_path,
         })
+
+        form_template = self.change_form_template
         if add and self.add_form_template is not None:
             form_template = self.add_form_template
-        else:
-            form_template = self.change_form_template
+
         context_instance = template.RequestContext(request, current_app=self.admin_site.name)
         return render_to_response(form_template or [
             "admin/%s/%s/change_form.html" % (app_label, opts.object_name.lower()),
