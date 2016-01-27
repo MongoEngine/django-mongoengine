78c57f857127d3f0ae30e009fc51dcb977549641
Sergey Tereschenko <serg.partizan@gmail.com>
2016-01-27 18:31:53 +0200

    views rewrite: override only required fields; changed "document" to "model" -> using same naming as django itself

--- django_mongoengine/views/__init__.py
+++ django_mongoengine/views/__init__.py
@@ -1,11 +1,13 @@
 from __future__ import absolute_import
 
-from django.views.generic.base import View, TemplateView, RedirectView
-
 from .detail import DetailView
 from .edit import FormView, CreateView, UpdateView, DeleteView, EmbeddedDetailView
 from .list import ListView
 
+__all__ = [
+    "DetailView", "FormView", "CreateView", "UpdateView", "DeleteView", "EmbeddedDetailView",
+    "ListView",
+]
 
 class GenericViewError(Exception):
     """A problem in a generic view."""
