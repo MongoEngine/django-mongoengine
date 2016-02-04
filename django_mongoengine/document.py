7a7bd06900bfaf62ba2e6a8c13fcafcd1b41713c
Sergey Tereschenko <serg.partizan@gmail.com>
2016-02-04 21:08:50 +0200

    django_meta update

--- django_mongoengine/document.py
+++ django_mongoengine/document.py
@@ -6,12 +6,20 @@ from mongoengine.base import metaclasses as mtc
 from .forms.document_options import DocumentMetaWrapper
 from .queryset import QuerySetManager
 
-def django_meta(meta, *bases):
+def django_meta(meta, base):
     class metaclass(meta):
-        def __new__(cls, name, this_bases, attrs):
+        def __new__(cls, name, bases, attrs):
             attrs.setdefault('objects', QuerySetManager())
             attrs.setdefault('_default_manager', QuerySetManager())
-            new_cls = meta.__new__(cls, name, bases, attrs)
+            change_bases = len(bases) == 1 and (
+                bases[0].__name__ == "temporary_meta" or
+                bases[0] in [Document, DynamicDocument, EmbeddedDocument]
+            )
+            if change_bases:
+                new_bases = base,
+            else:
+                new_bases = bases
+            new_cls = meta.__new__(cls, name, new_bases, attrs)
             new_cls._meta = DocumentMetaWrapper(new_cls)
             return new_cls
 
