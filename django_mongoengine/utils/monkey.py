0c8d8a5e8dffc2acd15da8e4fef76d4e04fa71ae
Sergey Tereschenko <serg.partizan@gmail.com>
2016-02-05 15:37:26 +0200

    monkeypatcher update

--- django_mongoengine/utils/monkey.py
+++ django_mongoengine/utils/monkey.py
@@ -4,10 +4,10 @@ import importlib
 def get_patched_django_module(modname, **kwargs):
     parent, m = modname.rsplit(".", 1)
     path = importlib.import_module(parent).__path__
-    m = imp.load_module(
-        "django_mongoengine.monkey.%s" % modname,
+    mod = imp.load_module(
+        ".".join([__name__, modname.replace(".", "_")]),
         *imp.find_module(m, path)
     )
     for k, v in kwargs.items():
-        setattr(m, k, v)
-    return m
+        setattr(mod, k, v)
+    return mod
