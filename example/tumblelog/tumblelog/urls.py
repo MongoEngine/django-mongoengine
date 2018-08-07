063dd8d95287707b9baef8e634dac605844534e1
Sergey Tereschenko <serg.partizan@gmail.com>
2018-08-07 16:51:51 +0300

    indent fix

--- example/tumblelog/tumblelog/urls.py
+++ example/tumblelog/tumblelog/urls.py
@@ -21,9 +21,9 @@ urlpatterns = [
     url(r'^$', PostIndexView.as_view(), name="post_index"),
     url(r'^new/$', AddPostView.as_view(), name="post_new"),
     url(r'^new/(?P<post_type>(post|video|image|quote|music))/$',
-            AddPostView.as_view(), name="post_new"),
+        AddPostView.as_view(), name="post_new"),
     url(r'^admin/', include(mongo_admin.site.urls)),
     url(r'^image-file/(?P<slug>[a-zA-Z0-9-]+)/', ImageFileView.as_view(),
-            name="image_file"),
+        name="image_file"),
     url(r'^(?P<slug>[a-zA-Z0-9-]+)/', include(post_patterns)),
 ]
