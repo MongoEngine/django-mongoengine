df847014c927d63a8c152a94c69562637537a977
Sergey Tereschenko <serg.partizan@gmail.com>
2019-04-14 12:09:06 +0300

    add context_processor to templates

--- example/tumblelog/tumblelog/settings.py
+++ example/tumblelog/tumblelog/settings.py
@@ -101,6 +101,16 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(os.path.realpath(os.path.dirname(__file__)), '../templates')],
         'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.request',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
