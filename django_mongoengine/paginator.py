68c74b9e0b4601b973a500fd82c3b29ed3cfd80a
Sergey Tereschenko <serg.partizan@gmail.com>
2020-03-05 13:17:35 +0200

    Fix list handling in Paginator

--- django_mongoengine/paginator.py
+++ django_mongoengine/paginator.py
@@ -6,4 +6,7 @@ class Paginator(Paginator):
 
     @cached_property
     def count(self):
-        return self.object_list.count()
+        try:
+            return self.object_list.count()
+        except TypeError:
+            return len(self.object_list)
