07ac71c049bb6a5d17bc014ae598563ec783ed91
Iliyass Hamza <iliyass.hamza@gmail.com>
2021-03-12 09:30:40 +0100

    refactor: Replace ugettext_lazy with gettext_lazy
    Fixes RemovedInDjango40Warning
    
    Refs #147

--- django_mongoengine/mongo_auth/models.py
+++ django_mongoengine/mongo_auth/models.py
@@ -1,5 +1,5 @@
 from django.utils.encoding import smart_str
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import (
