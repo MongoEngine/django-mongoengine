8fb28d2d5fc9bdca7e4b33522f4b04cffd5ec143
Serg Tereshchenko <serg.partizan@gmail.com>
2022-05-14 14:00:55 +0300

    chore: Fix deprecation for default_app_config

--- django_mongoengine/__init__.py
+++ django_mongoengine/__init__.py
@@ -1,6 +1,11 @@
-from .document import Document, DynamicDocument, EmbeddedDocument, DynamicEmbeddedDocument
+from .document import Document, DynamicDocument, DynamicEmbeddedDocument, EmbeddedDocument
 from .queryset import QuerySet, QuerySetNoCache
 
 __all__ = ["QuerySet", "QuerySetNoCache", "Document", "DynamicDocument", "EmbeddedDocument", "DynamicEmbeddedDocument"]
 
-default_app_config = 'django_mongoengine.apps.DjangoMongoEngineConfig'
+try:
+    from django import VERSION as _django_version
+    if _django_version < (3, 2):
+        default_app_config = 'django_mongoengine.apps.DjangoMongoEngineConfig'
+except ImportError:
+    pass
